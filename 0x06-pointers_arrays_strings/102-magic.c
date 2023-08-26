2cae66ec9e94a6e7b2ca08aee06a4fbaca9d4a4b
meseretnigusie <meseretzenigusie@gmail.com>
2023-08-25 18:26:05 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 
+/*
+ * main - Entry point
+ *
+ * Description: This function demonstrates a specific behavior by
+ *              assigning a value using pointer arithmetic.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-        int n;
-        int a[5];
-        int *p;
+	int n;
+	int a[5];
+	int *p;
 
-        a[2] = 1024;
-        p = &n;
-/*
- * write your line of code here...
- * Remember:
- * - you are not allowed to use a
- * - you are not allowed to modify p
- * - only one statement
- * - you are not allowed to code anything else than this line of code
- */
-        *(p + 5) = 98;
-        p[5] = 98; // Add this line 
-/* ...so that this prints 98\n */
-        printf("a[2] = %d\n", a[2]);
-        return (0);
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * Write your line of code here...
+	 * Remember:
+	 * - You are not allowed to use 'a'
+	 * - You are not allowed to modify 'p'
+	 * - Only one statement is allowed
+	 * - You are not allowed to code anything else than this line of code
+	 */
+	p[5] = 98; // Add this line
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
