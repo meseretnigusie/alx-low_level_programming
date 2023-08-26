32b62325ecadd3acda3b8068771ab41a993fac49
meseretnigusie <meseretzenigusie@gmail.com>
2023-08-26 12:51:00 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,21 +7,21 @@
  */
 void print_rev(char *s)
 {
-        int length = 0;
-        int i;
+	int length = 0;
+	int i;
 
-        while (*s != '\0')
-        {
-                length++;
-                s++;
-        }
-        s--;
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	s--;
 
-        for (i = length; i > 0; i--)
-        {
-                _putchar(*s);
-                s--;
-        }
+	for (i = length; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
 
-        _putchar('\n');
+	_putchar('\n');
 }
